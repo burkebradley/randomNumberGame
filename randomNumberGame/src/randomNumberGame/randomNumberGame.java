689245d8c2339aa286cba715bd35e455abaf6d09
burke.bradley <burke.bradley@MU-C1-04.mullen.pvt.k12.co.us>
2018-08-24 15:03:11 -0600

    firstFinalDraft

--- randomNumberGame/src/randomNumberGame/randomNumberGame.java
+++ randomNumberGame/src/randomNumberGame/randomNumberGame.java
@@ -13,43 +13,55 @@ public class randomNumberGame
 					System.out.println("What's your name?");
 					String userName = userInput.nextLine();
 					System.out.println("Hello " + userName);
-					System.out.println("What difficulty would you like?");
-					System.out.println("1) Easy (1-5");
-					System.out.println("2) Medium (1-10");
-					System.out.println("3) Hard (1-100");
-					int userDifficulty = userInput.nextInt();
-					int numberGuesses = 0;
-					boolean userGuessing;
-					int numberRange;
+					boolean stillPlaying;
+					while (stillPlaying = true) {
+						System.out.println("What difficulty would you like?");
+						System.out.println("1) Easy (1-5");
+						System.out.println("2) Medium (1-10");
+						System.out.println("3) Hard (1-100");
+						int userDifficulty = userInput.nextInt();
+						int numberGuesses = 0;
+						boolean userGuessing;
+						int numberRange;
 					
-					if (userDifficulty == 1) {
-						numberRange = 5;
-					}
-					else if (userDifficulty == 2) {
-						numberRange = 10;
-					}
-					else {
-						numberRange = 100;
-					}
-					int secretNumber = (int)(Math.random() * numberRange) + 1;
-					System.out.println(secretNumber);
-					System.out.println("Now try to guess the number.");
-						while (userGuessing = true) {
-							int userGuess = userInput.nextInt();
-							if (userGuess > secretNumber) {
-								System.out.println("Too high, try again!");
-								numberGuesses ++;
-							}
-							else if (userGuess < secretNumber) {
-								System.out.println("Too low, try again!");
-								numberGuesses ++;
-							}
-							else if (userGuess == secretNumber) {
-								numberGuesses ++;
-								System.out.println("Congradulations! It only took you " + numberGuesses +  " tries.");
-								stillGuessing = false;
-							}
-						
+						if (userDifficulty == 1) {
+							numberRange = 5;
+						}
+						else if (userDifficulty == 2) {
+							numberRange = 10;
+						}
+						else {
+							numberRange = 100;
+						}
+						int secretNumber = (int)(Math.random() * numberRange) + 1;
+						System.out.println(secretNumber);
+						System.out.println("Now try to guess the number.");
+							while (userGuessing = true) {
+								int userGuess = userInput.nextInt();
+								if (userGuess > secretNumber) {
+									System.out.println("Too high, try again!");
+									numberGuesses ++;
+								}
+								else if (userGuess < secretNumber) {
+									System.out.println("Too low, try again!");
+									numberGuesses ++;
+								}
+								else if (userGuess == secretNumber) {
+									numberGuesses ++;
+									System.out.println("Congradulations! It only took you " + numberGuesses +  " tries.");
+									stillGuessing = false;
+								}
+								System.out.println("Do you want to play again?");
+								System.out.println("1) Yes");
+								System.out.println("2) No");
+								int userPlayAgain = userInput.nextInt();
+								if (userPlayAgain == 1) {
+									stillPlaying = true;
+								}
+								else {
+									stillPlaying = false;
+								}
+						}
 					}
 				}
 			}
